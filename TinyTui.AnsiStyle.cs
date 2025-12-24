0b31f7dd2a43be8e7fe6d4b719cae30355637bd5
Alfred Broderick <AlfredBr@outlook.com>
2025-12-23 20:40:57 -0500

    checkpoint

--- TinyTui.AnsiStyle.cs
+++ TinyTui.AnsiStyle.cs
@@ -2,16 +2,16 @@ namespace AlfredBr;
 
 public static partial class TinyTui
 {
-	public enum AnsiStyle
-	{
-		Reset = 0,
-		Bold = 1,
-		Dim = 2,
-		Italic = 3,
-		Underline = 4,
-		Blink = 5,
-		Inverse = 7,
-		Hidden = 8,
-		Strike = 9
-	}
+    public enum AnsiStyle
+    {
+        Reset = 0,
+        Bold = 1,
+        Dim = 2,
+        Italic = 3,
+        Underline = 4,
+        Blink = 5,
+        Inverse = 7,
+        Hidden = 8,
+        Strike = 9
+    }
 }
